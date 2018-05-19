98b5905fca81ad3c78e4bcb335d3fd4a2e271732
Vincent Fretin <vincent.fretin@gmail.com>
2018-05-19 12:56:17 +0200

    add third coordinate in roundCoord

--- round-coord.js
+++ round-coord.js
@@ -2,5 +2,6 @@ module.exports = function roundCoord(c, precision) {
     return [
         Math.round(c[0] / precision) * precision,
         Math.round(c[1] / precision) * precision,
+        Math.round(c[2] / precision) * precision,
     ];
 };
